93dba3f69715bad06547b6859abe48a3c1ade197
Pradeep..!! <yankannavarpradeep@gmail.com>
2025-08-06 12:40:04 +0530

    Changed red to yellow in HomePage component

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,18 +7,18 @@ const HomePage = () => {
   return ( 
     <div className=' h-screen p-10 *:p-5'>
       <Link to="/Posts">
-        <button className='bg-red-200 border-2 rounded-xl p-5'>Posts</button>
+        <button className='bg-yellow-500 border-2 rounded-xl p-5'>Posts</button>
       </Link>
       <Link to="/Images">
-        <button className='bg-red-200 border-2 rounded-xl p-5'>Images</button>
+        <button className='bg-yellow-500 border-2 rounded-xl p-5'>Images</button>
       </Link>
       {/* vgb */}
       <Link to="/Comment">
-        <button className='bg-red-200 border-2 rounded-xl p-5'>Comment</button>
+        <button className='bg-yellow-500 border-2 rounded-xl p-5'>Comment</button>
       </Link>
       {/*  */}
       <Link to="/ToDolist">
-        <button className='bg-red-200 border-2 rounded-xl p-5'>ToDoList</button>
+        <button className='bg-yellow-500 border-2 rounded-xl p-5'>ToDoList</button>
       </Link>
 
     </div>
